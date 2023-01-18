0ad09600b01451ddd7a6edf0fd17b1b6b6f2f39d
rafana-tech <naufal.sabartudin@gmail.com>
2023-01-18 19:20:05 +0700

    origin

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  // res.header("Access-Control-Allow-Origin", req.headers.origin);
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept"
+  // );
   res.header(
     "Access-Control-Allow-Headers",
     "https://frontend-plantshop.vercel.app"
